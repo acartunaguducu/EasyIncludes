373c0efdb30e2df2c3a8e935fba45991e8c39105
Acar Tuna Güdücü <acartuna141406@gmail.com>
2025-07-19 23:35:21 +0300

    Create EasyIncludes.cpp

---
+++ EasyIncludes.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+using namespace std;
+
+void easyImport() {
+    cout<<"PLEASE TYPE IMPORTS SUCH AS: `iostream string vector sstream`"<<endl;
+    string importlar;
+    getline(cin, importlar);
+    istringstream iss(importlar);
+
+
+    string ss_importlar;
+
+    vector<string> imports;
+    while (iss >> ss_importlar) {
+        imports.push_back(ss_importlar);
+    }
+
+    
+    string str_imports;
+    for (int i = 0; i < imports.size(); i++) {
+        str_imports += "#include <" + imports[i] + ">\n";
+    }
+
+
+    cout << endl << str_imports << endl;
+
+
+    
+}
+
+
+int main() {
+   
+    easyImport();
+    
+}
